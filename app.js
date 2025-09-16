ed8613d795c07b8c16b0bc14cc02f2c983b0f034
saish shaikh <saishshaikh2323@gmail.com>
2025-09-16 15:06:50 +0530

    Day 1: js practice

---
+++ app.js
@@ -0,0 +1,18 @@
+////let sara = prompt("Write your note: ");
+
+//if (sara === "donky") {
+  //console.log("she is chutya");
+//} else {
+  //console.log("she is good");
+//}
+
+let price = prompt("Enter your popcorn price");
+if(price==="500"){
+  console.log("your popcorn is XXL");
+} else if (price==="400") {
+   console.log("your popcorn is XL");
+} else if ( price==="300") {
+  console.log("your popcorn size is X");
+} else if ( price==="200"){
+  console.log("your popcorn size is L");
+}
